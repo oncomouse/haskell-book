14f1313b7b5e36565aeef8c396053515c852fa45
oncomouse <oncomouse@gmail.com>
2020-11-03 15:03:18 -0600

    Initial Commit

---
+++ ch2.hs
@@ -0,0 +1,46 @@
+module Ch2 where
+
+sayHello :: String -> IO ()
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+main :: IO ()
+main = sayHello ("Andrew")
+
+triple :: Int -> Int
+triple x = x * 3
+
+piRSquared x = pi * x * x
+
+foo x = let y = x * 2
+            z = x * 5 in 2 * z * y
+
+mult1 = x * y
+  where
+    x = 5
+
+    y = 6
+
+-- let x = 3; y = 1000 in x * 3 + y
+firstEx = x * 3 + y
+  where
+    x = 3
+
+    y = 1000
+
+-- let y = 10; x = 10 * 5 + y in x * 5
+secondEx = x * 5
+  where
+    y = 10
+
+    x = 10 * 5 + y
+
+-- let x = 7
+--     y = negate x
+--     z = y * 10 in z / x + y
+thirdEx = z / x + y
+  where
+    x = 7
+
+    y = negate x
+
+    z = y * 10
